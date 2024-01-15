f0648960e2a4b1f91a6bc20e3ca888bf58881357
Konrad <konrad.babowicz21@gmail.com>
2024-01-15 21:51:20 +0000

    checking to see if works

--- src/DOM.js
+++ src/DOM.js
@@ -5,8 +5,12 @@ const dom = (() => {
 
     function addProject(){
         const title = document.getElementById("folder-name");
-        projects.projectsAppend(title)
+        projects.projectsAppend(title);
+        console.log(title);
+        return title;
     }
+
+    addProject();
 })
 
 export{dom};
