94bfd811f485c5868a95f2495694a2a8b57622f1
Konrad <konrad.babowicz21@gmail.com>
2024-01-16 21:46:44 +0000

    added notes on function

--- src/DOM.js
+++ src/DOM.js
@@ -1,7 +1,10 @@
 import { projects } from './folder.js';
 
 const dom = (() => {
+    //console log to check if dom is working
     console.log("project");
+    //function to take the input from dialog and store it into the projects array list
+    //this is done in the folder.js file
     function addProject(){
         const title = document.getElementById("folder-name");
         projects.projectsAppend(title);
@@ -10,7 +13,7 @@ const dom = (() => {
     }
 
     function createProject(){
-        
+
     }
 
 })
