45d371ed191d6519350a9e678efcbeb0dd754837
Konrad <konrad.babowicz21@gmail.com>
2024-01-17 22:37:50 +0000

    added createProject function

--- src/DOM.js
+++ src/DOM.js
@@ -12,8 +12,15 @@ const dom = (() => {
         return title;
     }
 
-    function createProject(){
-
+    //create the project, takes users input, takes the title, stores it into a div
+    //Makes sure that it creates a button, with the same title user inputs
+    function createProject(title){
+        const newP = document.createElement('div');
+        newP.classList.add('project-folder');
+        newP.innerHTML = `
+        <button class="project-btn" id="${title}">${title}</button>
+        <button><i class="fa-solid fa-trash project-remove"></i></button>
+        `
     }
 
 })
