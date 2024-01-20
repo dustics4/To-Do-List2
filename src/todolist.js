3e4395931b3e2d366398b6049ec492fbe6b9ab6c
Konrad <konrad.babowicz21@gmail.com>
2024-01-20 21:21:08 +0000

    created folder to do and task classes

--- src/todolist.js
+++ src/todolist.js
@@ -16,6 +16,22 @@ export default class ToDoList {
     }
 
     getProject(projectName){
-        return this.projects.find((project) => project.getTitle())
+        return this.projects.find((project) => project.getTitle() === projectName);
     }
+
+    contains(projectName){
+        return this.project.some((project) => project.getTitle() === projectName);
+    }
+
+    addProject(newProject){
+        if(this.projects.find((project) => project.name === newProject.name))
+        return
+        this.projects.push(newProject);
+    }
+
+    deleteProject(projectName){
+        const projectToDelete = this.projects.find((project) => project.getTitle() === projectName);
+        this.projects.splice(this.projects.indexOf(projectToDelete, 1));
+    }
+
 }
