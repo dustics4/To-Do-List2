88537e3abcd2f220a6f06fe6073052ecb738c1e7
Konrad <konrad.babowicz21@gmail.com>
2024-01-21 13:06:31 +0000

    starting from scratch again

--- src/DOM.js
+++ src/DOM.js
@@ -1,44 +1,12 @@
-import ToDoList from "./todolist";
-import Project from "./folder";
+
 
 const dom = (() => {
-
-    const projects = new ToDoList;
 
     const projectsDiv = document.getElementById("folder-body")
     const folderButtonSubmit = document.getElementById("folder-buttonSubmit");
 
-    //function to take the input from dialog and store it into the projects array list
-    //this is done in the folder.js file
-    function addProject(){
-        const title = document.getElementById("folder-name");
-        projects.title = title.value;
-        projects.addProject(title);
-        return title;
-    }
-
-    //create the project, takes users input, takes the title, stores it into a div
-    //Makes sure that it creates a button, with the same title user inputs
-    function createProject(title){
-        const newP = document.createElement('div');
-        newP.classList.add('project-folder');
-        newP.innerHTML = `
-        <button class="project-btn" id="${title}">${title}</button>
-        <button><i class="fa-solid fa-trash project-remove"></i></button>
-        `
-        return newP;
-    }
-
-    function displayProject(){
-        let list = projects.getProject;
-        projectsDiv.innerHTML = "";
-        list.forEach(project => projectsDiv.appendChild(createProject(project.name)));
-
-    }
 
     folderButtonSubmit.addEventListener('click', () =>{
-        addProject();
-        displayProject();
     })
 })
 
