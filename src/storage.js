a607a3b30cf57c4dc16bfbf1f7a15fef7fae9de4
dustics4 <konrad.babowicz21@gmail.com>
2024-02-13 20:24:14 +0000

    added functions save and load for local storage

--- src/storage.js
+++ src/storage.js
@@ -1,7 +1,17 @@
 //creating a local storage for the project so far
 
 const Storage = (() => {
+    function save(key,data){
 
+    }
+    function load(key){
+
+    }
+
+    return {
+        save,
+        load,
+    };
 })
 
 export default Storage;
